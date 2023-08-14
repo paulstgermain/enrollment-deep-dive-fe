73ef8145ca1cc53cafab42fee7add44c5cb9be30
paulstgermain <paulg.stgermain@gmail.com>
2023-08-14 12:56:19 -0400

    initial data grid setup, pulls data successfully from Context

--- components/graphs/Table/Table.jsx
+++ components/graphs/Table/Table.jsx
@@ -1,52 +1,75 @@
 import { DataGrid } from '@mui/x-data-grid'
+import { useData } from '@/context/dataContext';
+
+// const columns = [
+//     { field: 'id', headerName: 'ID', width: 70 },
+//     { field: 'firstName', headerName: 'First Name', width: 130 },
+//     { field: 'lastName', headerName: 'Last Name', width: 130 },
+//     {
+//         field: 'age',
+//         headerName: 'Age',
+//         type: 'number',
+//         width: 90
+//     },
+//     {
+//         field: 'fullName',
+//         headerName: 'Full Name',
+//         description: 'This column has a value getter and is not sortable.',
+//         sortable: false,
+//         width: 160,
+//         valueGetter: (params) =>
+//             `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+//     },
+// ]
 
 const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First Name', width: 130 },
-    { field: 'lastName', headerName: 'Last Name', width: 130 },
+    { field: 'id', headerName: 'ID', type: 'number', width: 30 },
+    { field: 'date_created', headerName: 'Date Created', width: 150 },
+    { field: 'enrollment_coach', headerName: 'Enrollment Coach', width: 150 },
+    { field: 'transcript', headerName: 'Transcript', width: 300 },
+    { field: 'fireflies_summary', headerName: 'Fireflies Summary', width: 300 },
     {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90
+        field: 'time_availability',
+        headerName: 'Availability',
+        type: 'boolean',
+        width: 130,
+        valueGetter: (params) => params.row.ec_checklist.time_availability
     },
     {
-        field: 'fullName',
-        headerName: 'Full Name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+        field: 'commitment_readiness',
+        headerName: 'Commitment',
+        type: 'boolean',
+        width: 130,
+        valueGetter: (params) => params.row.ec_checklist.commitment_readiness
+    },
+    {
+        field: 'why_join',
+        headerName: 'Why',
+        type: 'boolean',
+        width: 70,
+        valueGetter: (params) => params.row.ec_checklist.why_join
     },
 ]
 
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
-
   export default function DataTable() {
+    const data = useData()
     return (
-        <div style={{ height: 400, width: '90%' }}>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: { page: 0, pageSize: 5 },
-                    },
-                }}
-                pageSizeOptions={[5, 10]}
-                checkboxSelection
-            />
-        </div>
+        <>
+            {
+                data && <div style={{ height: 400, width: '90%' }}>
+                <DataGrid
+                    rows={data}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 5 },
+                        },
+                    }}
+                    pageSizeOptions={[5, 10]}
+                    checkboxSelection
+                />
+            </div>
+            }
+        </>
     )
   }
